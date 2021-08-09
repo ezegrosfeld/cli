17b9a8bac5e25def0533590557a29cb9575177f9
ezegrosfeld <grosfeldezekiel@gmail.com>
2021-08-09 20:10:59 -0300

    Refactoring messages

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -27,10 +12,9 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "grosf-gh",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application.`,
+	Use:   "cli",
+	Short: "CLI para el manejo de repositorios de la companía",
+	Long:  `CLI para el manejo de repositorios de la companía`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	/* Run: func(cmd *cobra.Command, args []string) {
@@ -61,7 +45,7 @@ func initConfig() {
 		// Search config in home directory with name ".grosf-gh" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".grosf-gh")
+		viper.SetConfigName(".ezegrosfeld-cli")
 		viper.SafeWriteConfig()
 	}
 
